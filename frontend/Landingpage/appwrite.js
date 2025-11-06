b9138ac38ae6ae543205611eaf00f091daddb09a
arush <helpraizeit@gmail.com>
2025-11-06 21:15:34 +0530

    Update appwrite.js

--- frontend/Landingpage/appwrite.js
+++ frontend/Landingpage/appwrite.js
@@ -1,21 +1,44 @@
-import { Client, Storage, ID } from "appwrite";
+// appwrite.js
+import { Client, Storage, Databases, ID, Query } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
-  .setProject("690cbc750001ae465885");
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setProject('690cbc750001ae465885');
 
-const storage = new Storage(client);
+export const storage = new Storage(client);
+export const databases = new Databases(client);
 
-export const uploadPDF = async (file) => {
-  return await storage.createFile(
-    "690cbd9100161fe0ef26",
-    ID.unique(),
-    file
-  );
+export const BUCKET_ID = '690cbd9100161fe0ef26';
+export const DATABASE_ID = '690cbd94000e313b9a47'; // YOUR DB ID
+export const COLLECTION_ID = 'tests';         // Create this collection
+
+// Upload file + save metadata
+export const uploadTest = async (file, className, subject, teacher, topic) => {
+    const fileResponse = await storage.createFile(BUCKET_ID, ID.unique(), file);
+    
+    await databases.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {
+            class: className,
+            subject: subject,
+            teacher: teacher,
+            topic: topic,
+            fileId: fileResponse.$id,
+            uploadedAt: new Date().toISOString()
+        }
+    );
+    
+    return fileResponse;
 };
 
-export const downloadPDF = (fileId) => {
-  return storage.getFileDownload("690cbd9100161fe0ef26", fileId);
+// Get all tests for a class
+export const getTestsForClass = async (className) => {
+    const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID,
+        [Query.equal('class', className)]
+    );
+    return response.documents;
 };
-
-export default storage;
